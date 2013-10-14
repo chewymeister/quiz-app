8f5d168bd350b256ae6209aa5ff8609045380337
Jonathan Tsang <chewymeister88@googlemail.com>
2013-10-14 22:39:59 +0100

    modified sessions helper

--- features/step_definitions/helpers/sessions.rb
+++ features/step_definitions/helpers/sessions.rb
@@ -8,7 +8,7 @@ module SessionHelpers
     fill_in "user[name]", with: name
     fill_in "user[email]", with: email
     fill_in "user[password]", with: password
-    fill_in "user[password_confirmation]", with: password_confirmation
+    fill_in "user[password_confirmation]", with: password_confirmaetion
     click_button "Sign Up"
   end
 
