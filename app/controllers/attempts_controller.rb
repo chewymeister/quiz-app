5a6b0f952ec7fe0cc54819cd1f6a9caef85e40ab
Jonathan Tsang <chewymeister88@googlemail.com>
2013-10-20 01:14:56 +0100

    attempts controller, quiz model score method refactored

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -4,15 +4,9 @@ class AttemptsController < ApplicationController
   end
 
   def create
-    # @params = params
     @quiz = Quiz.find params[:quiz_id]
     @questions = params[:questions]
     @score = @quiz.score @questions
-    # @quiz = Quiz.find params[:quiz_id]
-    # @question = Question.find params[:question_id].keys[0]
-    # @answer = params[:question_id].values
-    # @score = Quiz.score
-    # flash[:notice] = "Attempt created with quiz #{@quiz.title} question #{@question.query} and answer #{@answer}"
     flash[:notice] = "#{@score}"
     redirect_to '/'
   end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -4,16 +4,11 @@ class Quiz < ActiveRecord::Base
     accepts_nested_attributes_for :questions
 
     def score questions_hash
-      total = []
-      questions_hash.each do |question_id, attempt|
+      total_correct = 0
+      total = questions_hash.map do |question_id, attempt|
         question = Question.find question_id.to_i
-        if attempt == question.answer
-          total << :correct
-        else
-          total << :incorrect
-        end
+        total_correct += 1 if attempt == question.answer
       end
-      total_correct = total.select{ |score| score == :correct }.count.to_f
       "#{ (total_correct / total.count.to_f * 100).to_i }%"
     end
 
